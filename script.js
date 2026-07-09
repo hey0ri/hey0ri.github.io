e278aa6eceebd1e449ee164172cfad290d84d5ca
hey0ri <hey0ri@proton.me>
2026-07-09 02:15:21 +0200

    css + js

---
+++ script.js
@@ -0,0 +1,3 @@
+// Rien d'obligatoire ici : le site marche très bien sans JS.
+// Petit clin d'œil dans la console pour ceux qui regardent.
+console.log("%c👽 t'es pas censé être là, mais bienvenue quand même.", "color:#ff3cac; font-family:sans-serif; font-size:14px;");
